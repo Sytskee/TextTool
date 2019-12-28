e6fe53073e98aff3e02edb4133d8377d906f79a7
Joost <439100+jvnoije@users.noreply.github.com>
2019-12-28 14:58:14 +0100

    Added setings modal, not completely finished yet

--- static/js/app.js
+++ static/js/app.js
@@ -20,8 +20,19 @@ var settingsWebSocket = new WebSocket("ws://localhost:8888/api/v1/settings");
 
 settingsWebSocket.onmessage = function (event) {
     var settings = JSON.parse(event.data);
+    localStorage.setItem('settings', event.data);
+
     $("#webapp_settings").data(settings);
     $("#webapp_settings").text(event.data);
+
+    Object.keys(settings).forEach(function(key) {
+        var input = $("input#" + key)
+        if (input.attr("type") == "checkbox") {
+            input.prop("checked", settings[key])
+        } else {
+            input.val(settings[key])
+        }
+    })
 
     setStartStopButton($("#webapp_settings").data("classifier_running"));
 };
@@ -40,22 +51,62 @@ function handleStartStopClick(event) {
 }
 
 function setStartStopButton(classifier_running) {
-    if (classifier_running) {
-        $("a#start_stop").addClass("btn-danger");
-        $("a#start_stop").removeClass("btn-success");
-        $("a#start_stop").html('<i class="fas fa-stop mr-2"></i>Stop')
+    var start_stop = $("a#start_stop")
 
-        $("#logging").val('')
+    if (classifier_running) {
+        start_stop
+            .addClass("btn-danger")
+            .removeClass("btn-success")
+        start_stop.children("i")
+            .addClass("fa-stop")
+            .removeClass("fa-play")
+
+        start_stop.children("span").text("Stop")
+        //start_stop.html("<i class='fas fa-stop mr-2'></i>Stop")
+
+        $("#logging").val("")
     }
     else {
-        $("a#start_stop").removeClass("btn-danger");
-        $("a#start_stop").addClass("btn-success");
-        $("a#start_stop").html('<i class="fas fa-play mr-2"></i>Start')
+        start_stop
+            .addClass("btn-success")
+            .removeClass("btn-danger")
+        start_stop.children("i")
+            .addClass("fa-play")
+            .removeClass("fa-stop")
+
+        start_stop.children("span").text("Start")
+        //start_stop.html("<i class="fas fa-play mr-2"></i>Start")
     }
 }
 
 $(document).ready(function() {
-    $('#container_id').fileTree({ root: '/some/folder/' }, function(file) {
+    $("#container_id").fileTree({ root: "/some/folder/" }, function(file) {
         alert(file);
     });
 });
+
+$("div.portfolio-modal input").change(function() {
+    var dataChanged = false
+
+    $("div.portfolio-modal input").each(function() {
+        if ($(this).attr("type") == "checkbox") {
+            var inputValue = $(this).prop("checked")
+        } else {
+            var inputValue = $(this).val()
+        }
+
+        var settings = JSON.parse(localStorage.getItem("settings"))
+        var settingsValue = settings[$(this).attr("id")]
+
+        if (inputValue != settingsValue) {
+            dataChanged = true
+            return false // break 'each()' loop
+        }
+    })
+
+    if (dataChanged) {
+        $(this).closest("form").siblings("button.apply").removeClass("disabled");
+    } else {
+        $(this).closest("form").siblings("button.apply").addClass("disabled");
+    }
+}); 
